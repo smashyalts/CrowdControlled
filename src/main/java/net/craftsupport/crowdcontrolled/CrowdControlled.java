0dd032bc2b71decad6f564b151ab6b43149cdbc5
smashyalts <smashyalts@gmail.com>
2024-07-29 00:27:36 +0200

    Here

--- src/main/java/net/craftsupport/crowdcontrolled/CrowdControlled.java
+++ src/main/java/net/craftsupport/crowdcontrolled/CrowdControlled.java
@@ -1,14 +1,23 @@
 package net.craftsupport.crowdcontrolled;
 
+import net.craftsupport.crowdcontrolled.events.testEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class CrowdControlled extends JavaPlugin {
+import java.util.logging.Logger;
 
+public final class CrowdControlled extends JavaPlugin {
+    Logger log = getLogger();
+    PluginManager pm = getServer().getPluginManager();
     @Override
     public void onEnable() {
+    pm.registerEvents(new testEvent(), this);
         // Plugin startup logic
-
+        log.info("this plugin is under HEAVY development");
     }
+
+
+
 
     @Override
     public void onDisable() {
---
+++ src/main/java/net/craftsupport/crowdcontrolled/events/testEvent.java
@@ -0,0 +1,18 @@
+package net.craftsupport.crowdcontrolled.events;
+
+
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class testEvent implements Listener {
+
+   // Testing Attributes until we get subscriber/superchat events up and running
+    @EventHandler
+    public void onTest(PlayerInteractEvent e) {
+        Player player = e.getPlayer();
+        player.getAttribute(Attribute.GENERIC_SCALE).setBaseValue(player.getAttribute(Attribute.GENERIC_SCALE).getValue() + 1);
+    }
+}
