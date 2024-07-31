ef7dad0926721152adffbfef2064f1ee53b03d81
Skullian <133133667+Skullians@users.noreply.github.com>
2024-07-31 20:56:51 +0100

    Platform impl

--- src/main/java/net/craftsupport/crowdcontrolled/CrowdControlled.java
+++ src/main/java/net/craftsupport/crowdcontrolled/CrowdControlled.java
@@ -1,24 +1,26 @@
 package net.craftsupport.crowdcontrolled;
 
-import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
-import com.github.twitch4j.pubsub.ITwitchPubSub;
 import lombok.extern.log4j.Log4j2;
-import net.craftsupport.crowdcontrolled.events.TestEvent;
+import net.craftsupport.crowdcontrolled.command.CommandHandler;
+import net.craftsupport.crowdcontrolled.command.CommandTabCompleter;
+import net.craftsupport.crowdcontrolled.config.ConfigFileHandler;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Log4j2(topic = "CrowdControlled")
 public final class CrowdControlled extends JavaPlugin {
     PluginManager pm = getServer().getPluginManager();
+    public static ConfigFileHandler configHandler;
 
     @Override
     public void onEnable() {
-    pm.registerEvents(new TestEvent(), this);
+        LOGGER.info("Registering Configs.");
+        configHandler = new ConfigFileHandler();
+        configHandler.loadFiles(this);
 
-    LOGGER.info("this plugin is under HEAVY development");
-    ITwitchPubSub twitchPubSub = getServer().getServicesManager().load(ITwitchPubSub.class);
-    twitchPubSub.listenForSubscriptionEvents(new OAuth2Credential("", ""), "1111");
-
+        LOGGER.info("Registering Commands.");
+        getCommand("crowdcontrolled").setExecutor(new CommandHandler());
+        getCommand("crowdcontrolled").setTabCompleter(new CommandTabCompleter());
     }
 
     @Override
